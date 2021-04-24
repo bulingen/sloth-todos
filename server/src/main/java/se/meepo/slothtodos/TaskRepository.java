aaf4ca64d9ad9639e54ddaf33664f5a5a72c7e3c
Adam LeCorney <adamlecorney@gmail.com>
2021-04-24 19:22:56 +0200

    add endpoint for getting member's tasks

--- server/src/main/java/se/meepo/slothtodos/TaskRepository.java
+++ server/src/main/java/se/meepo/slothtodos/TaskRepository.java
@@ -1,7 +1,10 @@
 package se.meepo.slothtodos;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import se.meepo.slothtodos.model.Task;
 
 public interface TaskRepository extends CrudRepository<Task, Integer> {
+  List<Task> findByMemberId(Integer memberId);
 }
